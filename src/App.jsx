c1afb169c111b26a9ed11ea4373c8e55c6bd4104
Alfin123-stack <alfinkarin789@gmail.com>
2024-10-08 23:27:03 +0700

    use UseReducer for state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("invalid action type" + action.type);
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
